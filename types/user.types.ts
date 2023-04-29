bfed2a2b4cc2a89ff042c44a4c93b1fe16c55b0a
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-04-29 17:08:29 +0300

    define the user type

---
+++ types/user.types.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export namespace UserNS {
+    export interface User {
+        _id: mongoose.Schema.Types.ObjectId;
+        username: string;
+        email: string;
+        password: string;
+        role: string;
+        fullName: string;
+        phoneNumber: string;
+        image: string;
+    }
+}
