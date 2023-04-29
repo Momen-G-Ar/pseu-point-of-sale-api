c4502492935f84fbb445fec916244034e3fee6fd
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-04-29 22:10:24 +0300

    Create user model.ts & index.ts of the models

---
+++ models/user.models.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['manager', 'cashier'],
+        default: 'cashier',
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
