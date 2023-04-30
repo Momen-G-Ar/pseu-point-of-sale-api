2a5c4963303e695bf8d123d9edc00cd9954c5294
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-04-30 12:13:56 +0300

    POS-19 adding signup validation middleware

---
+++ middlewares/signup-validation.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction } from "express";
+import { UserNS } from "../types";
+import { User } from '../models/index';
+import express from 'express';
+
+const signupValidation = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    const user: UserNS.User = req.body;
+    const userSameWithEmail = await User.find({
+        email: { $eq: user.email }
+    })
+    const userWithSameUsername = await User.find({
+        email: { $eq: user.email }
+    })
+    if (userSameWithEmail) {
+        return res.status(400).send('invalud email')
+    }
+    if (userSameWithEmail) {
+        return res.status(400).send('invalid username')
+    }
+    next()
+}
+
+export default signupValidation;
