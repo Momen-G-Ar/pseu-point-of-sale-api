7393e03e86cf276a577596f064f9fb7cefbd2703
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-04-30 12:31:45 +0300

    refliecting review comments

--- middlewares/signup-validation.middleware.ts
+++ middlewares/signup-validation.middleware.ts
@@ -9,12 +9,12 @@ const signupValidation = async (req: express.Request, res: express.Response, nex
         email: { $eq: user.email }
     });
     const userWithSameUsername = await User.find({
-        email: { $eq: user.email }
+        username: { $eq: user.username }
     });
     if (userSameWithEmail) {
-        return res.status(400).send('invalud email');
+        return res.status(400).send('invalid email');
     }
-    if (userSameWithEmail) {
+    if (userWithSameUsername) {
         return res.status(400).send('invalid username');
     }
     next();
