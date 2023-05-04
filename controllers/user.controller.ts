55585d17bb4abba1ea3bb4f077b47c369214a5e7
abdullah-nuaimi <181004@ppu.edu.ps>
2023-05-04 12:16:35 +0300

    add loginUser function

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,29 +3,43 @@ import { User } from "../models";
 import { UserNS } from "../types";
 
 const addUser = async (user: UserNS.User) => {
-    let role = 'cashier';
-    const users = await User.find();
-    if (!users.length) {
-        role = 'manager';
-    }
-    const newUser = new User({
-        email: user.email,
-        password: user.password,
-        role,
-        fullName: user.fullName,
-        image: user.image,
-    });
+  let role = "cashier";
+  const users = await User.find();
+  if (!users.length) {
+    role = "manager";
+  }
+  const newUser = new User({
+    email: user.email,
+    password: user.password,
+    role,
+    fullName: user.fullName,
+    image: user.image,
+  });
 
-    return newUser.save()
-        .then(() => {
-            return true;
-        })
-        .catch((error: mongoose.Error) => {
-            console.error(error.message);
-            return false;
-        });
+  return newUser
+    .save()
+    .then(() => {
+      return true;
+    })
+    .catch((error: mongoose.Error) => {
+      console.error(error.message);
+      return false;
+    });
+};
+
+const loginUser = async (user: UserNS.User) => {
+  const findUser = await User.find({
+    email: user.email,
+    password: user.password,
+  });
+  if (findUser.length > 0) {
+    return findUser;
+  } else {
+    return false;
+  }
 };
 
 export default {
-    addUser,
+  addUser,
+  loginUser,
 };
