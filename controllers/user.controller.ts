dc717ca466ff7843c0be4654e0fbab3018d45cf1
Momen-G-Ar <cp.momen@gmail.com>
2023-05-04 12:28:25 +0300

    Remove password form response

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,43 +3,43 @@ import { User } from "../models";
 import { UserNS } from "../types";
 
 const addUser = async (user: UserNS.User) => {
-  let role = "cashier";
-  const users = await User.find();
-  if (!users.length) {
-    role = "manager";
-  }
-  const newUser = new User({
-    email: user.email,
-    password: user.password,
-    role,
-    fullName: user.fullName,
-    image: user.image,
-  });
-
-  return newUser
-    .save()
-    .then(() => {
-      return true;
-    })
-    .catch((error: mongoose.Error) => {
-      console.error(error.message);
-      return false;
+    let role = "cashier";
+    const users = await User.find();
+    if (!users.length) {
+        role = "manager";
+    }
+    const newUser = new User({
+        email: user.email,
+        password: user.password,
+        role,
+        fullName: user.fullName,
+        image: user.image,
     });
+
+    return newUser
+        .save()
+        .then(() => {
+            return true;
+        })
+        .catch((error: mongoose.Error) => {
+            console.error(error.message);
+            return false;
+        });
 };
 
 const loginUser = async (user: UserNS.User) => {
-  const findUser = await User.find({
-    email: user.email,
-    password: user.password,
-  });
-  if (findUser.length > 0) {
-    return findUser;
-  } else {
-    return false;
-  }
+    const findUser = await User.find({
+        email: user.email,
+        password: user.password,
+    }).select(['email', 'role', 'fullName', 'image']);
+    if (findUser.length > 0) {
+        return findUser;
+    } else {
+        return false;
+    }
 };
 
 export default {
-  addUser,
-  loginUser,
+    addUser,
+    loginUser,
 };
